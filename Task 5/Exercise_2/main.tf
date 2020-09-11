4ef13a361aa5539d3a8c813a95733f377867c837
chandc <chandc999@gmail.com>
2020-09-11 17:04:41 -0400

    updated lambda function in main.tf

--- Task 5/Exercise_2/main.tf
+++ Task 5/Exercise_2/main.tf
@@ -1,3 +1,5 @@
+# reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
+
 terraform {
   required_providers {
     aws = {
@@ -8,7 +10,7 @@ terraform {
 
 
 provider "aws" {
-  region          = "${var.aws_region}"
+  region          = var.aws_region
 }
 
 data "archive_file" "lambda_zip" {
@@ -19,8 +21,8 @@ data "archive_file" "lambda_zip" {
 #
 # setup an iam role to access Lambda
 #
-resource "aws_iam_role" "for_lambda" {
-  name = "iam_role_for_lambda"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
   assume_role_policy = <<EOF
 {
@@ -40,10 +42,59 @@ EOF
 }
 
 
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
+# If skipping this resource configuration, 
+# also add "logs:CreateLogGroup" to the IAM policy below.
+
+resource "aws_cloudwatch_log_group" "greet_lambda" {
+  name              = aws_lambda_function.greet_lambda.function_name
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "greet_lambda" {
   filename         = "lambda_function.zip"
   function_name    = "greet_lambda"
-  role             = "${aws_iam_role.for_lambda.arn}"
+  role             = aws_iam_role.iam_for_lambda.arn
   handler          = "greet_lambda.lambda_handler"
   runtime          = "python3.8"
+  environment {
+		variables = {
+			greeting = "Greeting AWS Lambda. Have a good day"
+		}
+	}
+  #depends_on = [
+  #  aws_iam_role_policy_attachment.lambda_logs,
+  # aws_cloudwatch_log_group.greet_lambda
+  # ]
+
 }
+
